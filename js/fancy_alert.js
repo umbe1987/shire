342cd349d0b465606586412728cd4453215722a9
Firstname Lastname <umbertofilippo@tiscali.it>
2018-10-12 23:12:55 +0200

    fancyAlert now uses CSS flexbox

--- js/fancy_alert.js
+++ js/fancy_alert.js
@@ -26,7 +26,7 @@ export function fancyAlert(content, alertType, headerText = 'Error') {
         var modal = document.getElementById('alertModal');
         // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("close")[0];
-        modal.style.display = "block";
+        modal.style.display = "flex";
         // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
             modal.style.display = "none";
