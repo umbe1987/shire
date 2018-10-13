9f0f3a425a1e704010891dce0ae2be4ba276d76e
Firstname Lastname <umbertofilippo@tiscali.it>
2018-10-13 17:57:00 +0200

    opacity slider removed from basemaps

--- js/opacity_slider.js
+++ js/opacity_slider.js
@@ -8,25 +8,30 @@ export function opacitySlider(ol_layers, class_layers) {
         var class_elem = class_layers[i];
         var elem_name = class_layers[i].innerText;
         for (var j = 0; j < ol_layers.length; ++j) {
-            var layer_title = ol_layers[j].values_["title"];
+            var current_layer = ol_layers[j];
+            var layer_title = current_layer.values_["title"];
             if (elem_name === layer_title) {
-                // create new li element
-                var li_elem = document.createElement("LI");
-                // create slider input elem
-                var input_slider = document.createElement("INPUT");
-                input_slider.setAttribute("class", "slider");
-                input_slider.setAttribute("type", "range");
-                input_slider.setAttribute("min", "0.0");
-                input_slider.setAttribute("max", "1.0");
-                input_slider.setAttribute("value", "1.0");
-                input_slider.setAttribute("step", "0.1");
-                // set id of slider elem to layer_title
-                input_slider.setAttribute("id", layer_title);
-                // add slider input to li elem
-                li_elem.appendChild(input_slider);
-                // add li with layer legend after layer li
-                insertAfter(li_elem, class_elem);
-                changeOpacity(input_slider, ol_layers);
+                // ignore basemaps (type === 'TILE')
+                if (current_layer.type === 'IMAGE') {
+                    // create new li element
+                    var li_elem = document.createElement("LI");
+                    // create slider input elem
+                    var input_slider = document.createElement("INPUT");
+                    input_slider.setAttribute("class", "slider");
+                    input_slider.setAttribute("type", "range");
+                    input_slider.setAttribute("min", "0.0");
+                    input_slider.setAttribute("max", "1.0");
+                    input_slider.setAttribute("value", "1.0");
+                    input_slider.setAttribute("step", "0.1");
+                    // set id of slider elem to layer_title
+                    input_slider.setAttribute("id", layer_title);
+                    // add slider input to li elem
+                    li_elem.appendChild(input_slider);
+                    // add li with layer legend after layer li
+                    insertAfter(li_elem, class_elem);
+                    changeOpacity(input_slider, ol_layers);
+                }
+
             }
         }
 
