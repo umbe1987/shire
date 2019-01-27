f628bc3d6504b441f4e75ea447baef79e2caf93f
Firstname Lastname <umbertofilippo@tiscali.it>
2019-01-27 18:03:28 +0100

    addition to get_info code in preparation for pagination

--- js/get_info.js
+++ js/get_info.js
@@ -21,22 +21,34 @@ function getInfoUrl(evt, view, lyr) {
     }
 }
 
-export function getInfo(evt, view, ol_layers) {
+// https://stackoverflow.com/a/53897629/1979665
+function getHTML(url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url, true);
+        xhr.responseType = 'document';
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status == 200) {
+                resolve(xhr.response.documentElement.innerHTML);
+            } else {
+                reject(status);
+            }
+        };
+        xhr.send();
+    });
+}
+
+export async function getInfo(evt, view, ol_layers) {
     // display info in fancyAlert at map's 'singleclick' events
     for (let i = 0; i < ol_layers.length; ++i) {
         let layer = ol_layers[i];
-        let text = getInfoUrl(evt, view, layer);
-        if (text) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    if (this.responseText.indexOf('</TABLE>') != this.responseText.lastIndexOf('</TABLE>')) {
-                        fancyAlert(this.responseText, 'info', 'Layer Info');
-                    }
-                }
-            };
-            xhttp.open("GET", text, true);
-            xhttp.send();
+        let gfi_url = getInfoUrl(evt, view, layer);
+        if (gfi_url) {
+            var remoteCode = await getHTML(gfi_url);
+            if (remoteCode.indexOf('</table>') != remoteCode.lastIndexOf('</table>')) {
+                fancyAlert(remoteCode, 'info', 'Layer Info');
+            }
         }
     }
 }
