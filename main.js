776cced611f2f6c8ff0902c06b54394af3098e85
Firstname Lastname <umbertofilippo@tiscali.it>
2019-08-07 18:45:17 +0200

    Added CORS Anywhere to main.js (will install in my server in production)

--- main.js
+++ main.js
@@ -57,6 +57,26 @@ import {
     print_map
 } from './js/print/wm_print';
 
+// CORS ANYWHERE
+
+// https://github.com/Rob--W/cors-anywhere/#client
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
 // TOGGLE TITLE BAR
 toggleTitle();
 
